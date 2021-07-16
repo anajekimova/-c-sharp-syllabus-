396c87fb2dd637c8b2626cbd373532f8833d9147
Ana Dika <ana.jekimova@gmail.com>
2021-07-16 10:20:57 +0300

    Exercise 8

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double  minutes;
+            Console.WriteLine("Enter minute");
+            minutes = double.Parse(Console.ReadLine());
+            double hours = minutes / 60;
+            double days = hours / 24;
+            double  years = days / 365;
+
+            
+             
+            Console.WriteLine($"Approximately it will be {days.ToString("0")} days  and {years.ToString("0")} years.");
+        }
+    }
+}
