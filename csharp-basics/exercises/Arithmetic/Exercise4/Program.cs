df2d4f2f375dcafeb86a7e607366fd140a4fa51e
Ana Dika <ana.jekimova@gmail.com>
2021-07-20 10:31:19 +0300

    Exercise 4.

---
+++ csharp-basics/exercises/Arithmetic/Exercise4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long intergar = 1L;
+            int n = 10;
+
+            for(int i = 1; i <= n; i++)
+            {
+                intergar *= i;
+            }
+
+            Console.WriteLine(intergar); 
+        }
+    }
+}
