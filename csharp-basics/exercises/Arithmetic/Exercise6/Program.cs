3a810485d5c88ce0da4e33ad6de74def2994dc6c
Ana Dika <ana.jekimova@gmail.com>
2021-07-20 15:47:47 +0300

    Exercise6

---
+++ csharp-basics/exercises/Arithmetic/Exercise6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 111; i++)
+            {
+                if (i % 11 == 0)
+                    Console.WriteLine("");
+
+                if (i % 15 == 0)
+                    Console.WriteLine("CozaLoza");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Coza");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Loza");
+                else if (i % 7 == 0)
+                    Console.WriteLine("Woza");
+                else if (i % 3 != 0 || i % 5 != 0 || i % 7 != 0)
+                    Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
+
