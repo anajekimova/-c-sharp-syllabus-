f9db4d9198a9418812345df21b54e3889e350519
Ana Dika <ana.jekimova@gmail.com>
2021-07-21 16:09:40 +0300

    Exercise 9

---
+++ csharp-basics/exercises/Arithmetic/Exercise9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double weight;
+            double height;
+
+            Console.WriteLine("Enter your weight in kg: ");
+            weight = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entere your height in m: ");
+
+            height = double.Parse(Console.ReadLine());
+            double bmi = weight / (height * height);
+            if (bmi >= 18.5 || bmi <= 24.9)
+                Console.WriteLine($"Bmi is healthy.");
+            else if (bmi < 18.5)
+                Console.WriteLine($"BMI is underweight.{bmi}");
+            else if (bmi >= 24.9 || bmi < 30)
+                Console.WriteLine($"BMI is overweight. ");
+        }
+    }
+}
