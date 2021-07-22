81b0336739aec5f7177340c7a40066223e0993fe
Ana Dika <ana.jekimova@gmail.com>
2021-07-22 12:19:51 +0300

    Exercise 2

---
+++ csharp-basics/exercises/FlowOfControl/FlowControl/Exercise2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PositiveNegativeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the number.");
+            int input = int.Parse(Console.ReadLine());
+
+            if (input > 0)
+            {
+                Console.WriteLine("Number is positive");
+            }
+            else if (input < 0)
+            {
+                Console.WriteLine("Number is negative");
+            }
+            else
+            {
+                Console.WriteLine("Number is zero");
+            }
+        }
+
+    }
+}
