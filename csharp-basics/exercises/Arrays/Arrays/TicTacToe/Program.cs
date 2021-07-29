b44ff83b570591c3be6906e639df55bb66212a9c
Ana Dika <ana.jekimova@gmail.com>
2021-07-29 22:53:34 +0300

    Exercise 7 fix code

--- csharp-basics/exercises/Arrays/Arrays/TicTacToe/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/TicTacToe/Program.cs
@@ -77,16 +77,12 @@ namespace TicTacToe
         }
         private static void DisplayBoard()
         {
-            Console.WriteLine("\n");
-            Console.WriteLine("       0   1   2  ");
-            Console.WriteLine("     +---+---+---+");
-            Console.WriteLine("  0  | " + board[0, 0] + " | " + board[0, 1] + " | " + board[0, 2] + " |");
-            Console.WriteLine("     +---+---+---+");
-            Console.WriteLine("  1  | " + board[1, 0] + " | " + board[1, 1] + " | " + board[1, 2] + " |");
-            Console.WriteLine("     +---+---+---+");
-            Console.WriteLine("  2  | " + board[2, 0] + " | " + board[2, 1] + " | " + board[2, 2] + " |");
-            Console.WriteLine("     +---+---+---+");
-            Console.WriteLine("\n");
+            Console.WriteLine("  0  " + board[0, 0] + "|" + board[0, 1] + "|" + board[0, 2] + " |");
+            Console.WriteLine("    --+-+--");
+            Console.WriteLine("  1  " + board[1, 0] + "|" + board[1, 1] + "|" + board[1, 2] + " |");
+            Console.WriteLine("    --+-+--");
+            Console.WriteLine("  2  " + board[2, 0] + "|" + board[2, 1] + "|" + board[2, 2] + " |");
+            Console.WriteLine("    --+-+--");
         }
     }
 }
