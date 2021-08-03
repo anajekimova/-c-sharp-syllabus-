0183492cd54d7141855262f4bda89fcff1817fb9
Ana Dika <ana.jekimova@gmail.com>
2021-08-03 22:26:06 +0300

    Exercise 9

---
+++ csharp-basics/exercises/Loops/Loops/Exercise9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise9
+{
+    class RollTwoDice
+    {
+        static void Main(string[] args)
+        {
+            int sum;
+            Console.WriteLine("Enter sum from 2 to 12:");
+            sum = int.Parse(Console.ReadLine());
+            int result;
+
+            do
+            {
+                Random rand = new Random();
+                int nums1 = rand.Next(1, 7);
+                int nums2 = rand.Next(1, 7);
+                result = nums1 + nums2;
+                Console.WriteLine($"{nums1} and {nums2} = {result}");
+            } while (sum != result);
+            Console.ReadKey();     
+        }
+    }
+}
