892526dc1d9434e841361fab791655edf205ec93
Ana Dika <ana.jekimova@gmail.com>
2021-08-03 18:36:37 +0300

    Exercise 6

---
+++ csharp-basics/exercises/Loops/Loops/Exercise6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Enter a number:");
+            num = int.Parse(Console.ReadLine());
+
+            for(var i= 1; i< num; i++)
+            {
+                if(num % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+                else if(num % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+                else if(num % 15 == 0)
+                {
+                    Console.Write("FizzBuzz");
+                }
+                else
+                {
+                    Console.Write(i + " ");
+                }
+                if (num % 20 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
