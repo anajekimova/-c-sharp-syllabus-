3650521410ed09ae9bfb1eca993ef91fe62a26e2
Ana Dika <ana.jekimova@gmail.com>
2021-08-11 12:41:40 +0300

    Finished

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise1/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise1
+{
+    class Product
+    {
+        private string _name;
+        private double _priceAtStart;
+        private int _amountAtStart;
+
+        public Product(string name, double price, int amount)
+        {
+            _name = name;
+            _priceAtStart = price;
+            _amountAtStart = amount;
+        }
+
+        public void ChangePrice(double price)
+        {
+            _priceAtStart = price;
+        }
+
+        public void ChangeAmount(int amount)
+        {
+            _amountAtStart = amount; 
+        }
+
+        public void PrintPrice()
+        {
+            Console.WriteLine($"{_name}, {_priceAtStart} EUR, {_amountAtStart} units.");
+        }
+    }
+}
