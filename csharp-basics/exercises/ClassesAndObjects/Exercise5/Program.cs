2e04f305d764b6cfbabe0bd7169249445de2e56b
Ana Dika <ana.jekimova@gmail.com>
2021-08-12 14:37:41 +0300

    Finished

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day, month, year;
+
+            Console.WriteLine("Enter day:");
+            day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter month:");
+            month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter year:");
+            year = int.Parse(Console.ReadLine());
+
+            Date DateTest = new Date(day, month, year);
+            Console.WriteLine(DateTest.PrintDays());
+        }
+    }
+}
