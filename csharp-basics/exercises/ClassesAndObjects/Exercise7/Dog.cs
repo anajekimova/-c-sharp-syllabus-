e2b772fa500a1b27753ff45069161d47bb1065cc
Ana Dika <ana.jekimova@gmail.com>
2021-08-13 19:01:57 +0300

    Finished

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise7/Dog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise7
+{
+    class Dog
+    {
+
+        private string _name;
+        private string _sex;
+        private string _mother;
+        private string _father;
+
+        public Dog(string name, string sex, string mother, string farher)
+        {
+            _name = name;
+            _sex = sex;
+            _mother = mother;
+            _father = farher;
+        }
+
+        public void FatherName()
+        {
+            if(_name == null)
+            {
+                Console.WriteLine("Unknown");
+            }
+            else
+            {
+                Console.WriteLine(_father);
+            }
+        }
+
+        public void HasSameMotherAs(Dog name)
+        {
+            if(_mother == name._mother)
+            {
+                Console.WriteLine($"{_name} and {name._name} has a same mother.");
+            }
+            else
+            {
+                Console.WriteLine($"{_name} and {name._name} doesn't have a same mother. ");
+            }
+        }
+
+        public string PrintDog()
+        {
+            return $"{ _name} has {_mother} as mother, and  {_father} as father .";
+        }
+    }
+}
