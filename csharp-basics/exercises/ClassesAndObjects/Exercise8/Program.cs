f6316d4ef9412e63d84cfb7eb1e3d8b2c94f4a1d
Ana Dika <ana.jekimova@gmail.com>
2021-08-14 16:17:02 +0300

    finished

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Exercise8
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                double startMoney;
+                Console.WriteLine("How much money is in the account?");
+                startMoney = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the annual interest rate:");
+                int interest = int.Parse(Console.ReadLine());
+                var account = new SavingsAccount(interest, startMoney);
+                Console.WriteLine("How long has the account been opened?");
+                int month = int.Parse(Console.ReadLine());
+
+                for (int i = 0; i < month; i++)
+                {
+                    Console.WriteLine($"Enter amount deposited for month {i + 1}");
+                    int deposite = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Enter amount withdrawn for {i + 1}");
+                    double withDrawn = double.Parse(Console.ReadLine());
+
+                    account.Deposite(deposite);
+                    account.WithDrawn(withDrawn);
+                    account.Interests();
+                }
+
+                account.TotalDeposited();
+                account.TotalWithdrawn();
+                account.TotalInterest();
+                account.TotalBalnce();
+                Console.ReadLine();
+            }
+        }
+    }
+
+    
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise8/SavingsAccount.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise8
+{
+    class SavingsAccount
+    {
+        private double _startMoney;
+        private int _interest;
+        private double _totalWithDrawn;
+        private double _totalDeposide;
+        private double _totalBalance;
+        private double _balance;
+        private double _totalInterest;
+        
+
+        
+        public SavingsAccount(int interest, double startMoney)
+        {
+            _interest = interest;
+            _startMoney = startMoney;
+        }
+
+        public void Deposite(double money)
+        {
+            _balance += money;
+            _totalDeposide += money;
+        }
+
+        public void WithDrawn(double money)
+        {
+            _balance -= money;
+            _totalWithDrawn += money;
+        }
+
+        public void Balance()
+        {
+
+            _totalBalance += _balance;
+        }
+
+        public void Interests()
+        {
+            _balance += _balance * _interest / 12;
+            _totalInterest += _balance * _interest / 12;
+        }
+
+        public void TotalDeposited()
+        {
+            Console.WriteLine($"Total deposited: $ {_totalDeposide}");
+        }
+
+        public void TotalWithdrawn()
+        {
+            Console.WriteLine($"Total withdrawn: $ {_totalWithDrawn}");
+        }
+
+        public void TotalInterest()
+        {
+            Console.WriteLine($"Interest earned: $ {_totalInterest.ToString("0.00")} ");
+        }
+
+        public void TotalBalnce()
+        {
+            Console.WriteLine($"Ending balance:$ {_totalBalance}");
+        }
+    }
+}
+
