318f31bb552254d7edee1dba7d9d67890bc0b433
Ana Dika <ana.jekimova@gmail.com>
2021-08-14 16:49:31 +0300

    FInished

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise9/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise9
+{
+    class Point
+    {
+        private double _x;
+        private double _y;
+
+        public Point(double x, double y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public void SwapPoints(Point p1, Point p2)
+        {
+            double change1 = p1._x;
+            double change2 = p1._y;
+
+            p1._x = p2._x;
+            p1._y = p2._y;
+
+            p1._x = change1;
+            p2._y = change2;
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(5, 2);
+            Point p2 = new Point(-3, 6); 
+
+            Console.WriteLine($"({p1._x}, {p1._y})");
+            Console.WriteLine($"({p2._x}, {p2._y})");
+            Console.WriteLine("\n");
+
+            Point.SwapPoints(Point p1, Point p2);
+
+            Console.WriteLine($"({p1._x}, {p1._y})");
+            Console.WriteLine($"({p2._x}, {p2._y})");
+        }
+    }
+}
