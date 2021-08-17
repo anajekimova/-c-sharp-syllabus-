7ae05070b69c59df4359b18f2799d964a3b962a0
Ana Dika <ana.jekimova@gmail.com>
2021-08-17 16:49:36 +0300

    Finished

--- csharp-basics/exercises/Collections/Histogram/Program.cs
+++ csharp-basics/exercises/Collections/Histogram/Program.cs
@@ -9,15 +9,36 @@ namespace Histogram
 {
     class Program
     {
-        private const string Path = "../Histogram/midtermscores.txt";
+        private const string Path = "../../midtermscores.txt";
 
         private static void Main(string[] args)
         {
-            var readText = File.ReadAllLines(Path);
-            foreach (var s in readText)
+            var readText = File.ReadAllText(Path);
+            var input = readText.Split(' ');
+            var point = new List<string>();
+            point.Add("00-09");
+            point.Add("10-19");
+            point.Add("20-29");
+            point.Add("30-39");
+            point.Add("40-49");
+            point.Add("50-59");
+            point.Add("60-69");
+            point.Add("70-79");
+            point.Add("80-89");
+            point.Add("90-99");
+            point.Add("100");
+
+            foreach (var s in input)
             {
-                Console.WriteLine(s);
+                var nums = int.Parse(s) / 10;
+                point[nums] += "*";
             }
+
+            foreach(var k in point)
+            {
+                Console.WriteLine(k);
+            }
+            Console.ReadLine();
         }
     }
 }
