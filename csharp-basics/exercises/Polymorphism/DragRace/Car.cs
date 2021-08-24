0b59aa1bfab5a0271d772b5afc44d86b1963dbc2
Ana Dika <ana.jekimova@gmail.com>
2021-08-24 17:36:39 +0300

    Finished

---
+++ csharp-basics/exercises/Polymorphism/DragRace/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DragRace
+{
+    interface Car
+    {
+        string CarName();
+        void SpeedUp();
+        void SlowDown();
+        string ShowCurrentSpeed();
+        void StartEngine();
+    }
+}
---
+++ csharp-basics/exercises/Polymorphism/DragRace/Toyota.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DragRace
+{
+    public class Toyota : Car, Icar
+    {
+        private int _currentSpeed = 0;
+
+        public string CarName()
+        {
+            return "Toyota";
+        }
+
+        public void SpeedUp()
+        {
+            _currentSpeed += 25;
+        }
+
+        public void SlowDown()
+        {
+            _currentSpeed -= 15;
+        }
+
+        public string ShowCurrentSpeed()
+        {
+            return _currentSpeed.ToString();
+        }
+
+        public void UseNitrousOxideEngine()
+        {
+            _currentSpeed += 50;
+        }
+
+        public void StartEngine()
+        {
+            Console.WriteLine("Rrrrrrr.....");
+        }
+    }
+}
