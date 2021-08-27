cd7aed3b21de076baf8a99340a9c943cb0938076
Ana Dika <ana.jekimova@gmail.com>
2021-08-27 11:49:33 +0300

    Finished

--- csharp-basics/exercises/Arithmetic/Exercise6/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise6/Program.cs
@@ -2,27 +2,33 @@
 
 namespace Exercise6
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             for (int i = 1; i <= 111; i++)
             {
+                Console.WriteLine(" " + ColoLoza(i));
                 if (i % 11 == 0)
-                    Console.WriteLine("");
-
-                if (i % 15 == 0)
-                    Console.WriteLine("CozaLoza");
-                else if (i % 3 == 0)
-                    Console.WriteLine("Coza");
-                else if (i % 5 == 0)
-                    Console.WriteLine("Loza");
-                else if (i % 7 == 0)
-                    Console.WriteLine("Woza");
-                else if (i % 3 != 0 || i % 5 != 0 || i % 7 != 0)
-                    Console.WriteLine($"{i}");
+                    Console.WriteLine("");                
             }
+        }
+
+        public static string ColoLoza(int i)
+        {
+            string write = "";
+            if (i % 15 == 0)
+                write += "CozaLoza";
+            else if (i % 3 == 0)
+                write += "Coza";
+            else if (i % 5 == 0)
+                write +="Loza";
+            else if (i % 7 == 0)
+               write += "Woza";
+            else if (i % 3 != 0 || i % 5 != 0 || i % 7 != 0)
+                write += $"{i}";
+
+            return write;
         }
     }
 }
-
