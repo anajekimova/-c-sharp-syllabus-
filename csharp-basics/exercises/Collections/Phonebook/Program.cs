6f43c76ebc1276cc97011b1e354817ff4a47c4a6
Ana Dika <ana.jekimova@gmail.com>
2021-08-30 14:50:52 +0300

    Finished

--- csharp-basics/exercises/Collections/Phonebook/Program.cs
+++ csharp-basics/exercises/Collections/Phonebook/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PhoneBook;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,21 @@ using System.Threading.Tasks;
 
 namespace Phonebook
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            var name = new PhoneDirectory("Nika", 862229478);
+            var name1 = new PhoneDirectory("Alisa", 783720987);
+            var nam2 = new PhoneDirectory("Nikol", 678439876);
+
+            name.PrintNameNumber();
+            PhoneDirectory.Print();
+            PhoneDirectory.Print();
+            PhoneDirectory.GetNumber("Nika");
+            PhoneDirectory.GetNumber("Nikol");
+
+            Console.ReadKey();
         }
     }
 }
